f10718865fe5ab25fce7050c4988645d1af48bd5
mrmenand <1006024749@qq.com>
2019-06-19 16:10:34 +0800

    6.19

--- Algorithms/Java/LinkedList/LinkedList.java
+++ Algorithms/Java/LinkedList/LinkedList.java
@@ -116,6 +116,23 @@ public class LinkedList<E> {
 
 	}
 
+	public void removeElement(E e){
+
+		Node prev = dummyHead;
+		while(prev.next != null){
+			if(prev.next.e.equals(e))
+				break;
+			prev = prev.next;
+		}
+
+		if(prev.next != null){
+			Node delNode = prev.next;
+			prev.next = delNode.next;
+			delNode.next = null;
+			size --;
+		}
+	}
+
 	public E  removeFirst(){
 		return remove(0);
 	}
@@ -123,6 +140,8 @@ public class LinkedList<E> {
 	public E removeLast(){
 		return remove(size-1);
 	}
+
+
 
 	@Override
 	public String toString(){
---
+++ Algorithms/Java/Set/UniqueMorse.java
@@ -0,0 +1,16 @@
+import java.util.TreeSet;
+
+public class UniqueMorse {
+	public int UniqueMorse(String[] words){
+		String[] codes = {".-","-...","-.-.","-..",".","..-.","--.","....", "",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+		TreeSet<String> set = new TreeSet<>() ;
+		for(String word : words) {
+			StringBuilder res = new StringBuilder();
+			for (int i = 0; i < word.length(); i++)
+				res.append(codes[word.charAt(i) - 'a']);
+			set.add(res.toString());
+		}
+		return set.size();
+	}
+
+}
